03b52c53433eaac92f10a33bb3a979e68eea0b04
Puneeth2410 <84957587+Puneeth2410@users.noreply.github.com>
2021-08-09 19:51:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+function createApples(){
+  apple = createSprite(random(50,350),40,10,10);
+  apple.addImage(appleImage)
+  apple.scale= 0.5; 
+  apple.velocityY= 5;
+  apple.lifetime= 60;
+
+  var select_sprite = Math.round(random(1,2))
+
+  if(frameCount % 80 == 0){
+  if(select_sprite == 1){
+   function createApples()
+  }
+ else{ 
+   function createLeaves()
+ }
+}
+}
+}
